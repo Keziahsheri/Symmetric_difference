e63a29894c205961501ae076bf524baf37ff9aeb
Keziah <120482218+Keziahsheri@users.noreply.github.com>
2023-03-12 16:10:24 +0300

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello_world'
-
-if __name__ == '__main__':
-    app.run(debug = True,host="127.0.0.1", port = 5000)
